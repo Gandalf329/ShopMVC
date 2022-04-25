2cb04754d039778b12c9fa2d9522267ea52ae7c4
Gandalf329 <pohlebaev.n@yandex.ru>
2022-04-25 23:04:19 +0300

    Add user profile

---
+++ ViewModels/ViewUserProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ShopMVC.ViewModels
+{
+    public class ViewUserProfileModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public int Year { get; set; }
+        public int Money { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
